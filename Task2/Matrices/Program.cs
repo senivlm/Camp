1a0360dd62a2775e4bd8c023f2cb22fdf8b2d3c3
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-05-21 15:54:44 +0300

    Add different input options for Matrix class (vertical, diagonal, spiral snake and task from entry test)

--- Task2/Matrices/Program.cs
+++ Task2/Matrices/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Matrices;
+
+Matrix m1 = new Matrix(3, 4);
+
+m1.VerticalSnake();
+m1.PrintMatrix();
+
+Matrix m2 = new Matrix(4);
+
+m2.DiagonalSnake();
+m2.PrintMatrix();
+
+Matrix m3 = new Matrix(3, 4);
+
+m3.SpiralSnake();
+m3.PrintMatrix();
+
+Matrix m4 = new Matrix(9);
+m4.FirstStageTask();
+m4.PrintMatrix();
+
+Console.ReadKey();

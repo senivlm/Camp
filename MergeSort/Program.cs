aefd9489a3ea01f2595757a2f5f33a311560ab5b
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-06-01 18:06:36 +0300

    Add simple merge sort

---
+++ MergeSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using Task4;
+
+Vector v = new Vector(new int[] { 4, 7, 9, 6, 5, 4, 3, 2, 1, 4, 5, 6, 7, 8, 8434, 1, 3, 5, 45, });
+
+Console.WriteLine("Initial vector:\n" + v);
+
+Vector.MergeSort(v, 0, v.Lenght - 1);
+
+Console.WriteLine("\nSorted vector:\n" + v);
+
+Console.ReadKey();
---
+++ MergeSort/Vector.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Vector
+    {
+        private int[] _array;
+        public int Lenght => _array.Length;
+
+        public Vector(int[] arr)
+        {
+            _array = arr;
+        }
+        public Vector(int n)
+        {
+            _array = new int[n];
+        }
+        public Vector()
+        {
+            _array = Array.Empty<int>();
+        }
+
+        public int this[int index]
+        {
+            get => _array[index];
+            set => _array[index] = value;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(' ', _array);
+        }
+
+        public void RandomInitialization(int min, int max)
+        {
+            Random random = new Random();
+            for (int i = 0; i < _array?.Length; i++)
+            {
+                _array[i] = random.Next(min, max);
+            }
+        }
+
+        public void InitShuffle()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = i + 1;
+            }
+            Random random = new Random();
+            _array = _array.OrderBy(x => random.Next()).ToArray();
+        }
+        public Pair[] CalculateFrequency()
+        {
+            var res = _array
+                .GroupBy(x => x)
+                .Select(x => new Pair(x.Key, x.Count()))
+                .ToArray();
+
+            return res;
+        }
+
+        public bool IsPalindrome()
+        {
+            bool isPalindrom = true;
+            for (int i = 0; i < _array?.Length / 2; i++)
+            {
+                if (_array[i] != _array[_array.Length - 1 - i])
+                {
+                    isPalindrom = false;
+                    break;
+                }
+            }
+            return isPalindrom;
+        }
+        public void Reverse()
+        {
+            int lenght = _array.Length / 2;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                (_array[i], _array[lenght - 1 - i]) = (_array[lenght - 1 - i], _array[i]);
+            }
+        }
+        public string FindLongestSequence()
+        {
+            int? resNum = null;
+            int maxCount = 1;
+            int count = 1;
+
+            for (int i = 0; i < _array.Length - 1; i++)
+            {
+                if (_array[i] == _array[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        resNum = _array[i];
+                        maxCount = count;
+                    }
+                    count = 1;
+                }
+            }
+
+            if (resNum != null)
+                return $"Number {resNum} occurs {maxCount} times in a row.";
+            else
+                return "Every number occurs only once.";
+        }
+
+        #region Sort
+
+        public static void QuickSort(Vector vec, int start, int end)
+        {
+            if (start < end)
+            {
+                int pivotIdx = Partition(vec, start, end);
+
+                QuickSort(vec, start, pivotIdx - 1);
+                QuickSort(vec, pivotIdx + 1, end);
+            }
+        }
+
+        private static int Partition(Vector vec, int start, int end)
+        {
+            int pivot = vec[end];
+            int i = start - 1;
+
+            for (int j = start; j < end; j++)
+            {
+                if (vec[j] < pivot)
+                {
+                    (vec[++i], vec[j]) = (vec[j], vec[i]);
+                }
+            }
+            (vec[i + 1], vec[end]) = (vec[end], vec[i + 1]);
+
+            return i + 1;
+        }
+
+        public static void MergeSort(Vector vec, int l, int r)
+        {
+            if (l < r)
+            {
+                int middlePoint = l + (r - l) / 2;
+
+                MergeSort(vec, l, middlePoint);
+                MergeSort(vec, middlePoint + 1, r);
+
+                Merge(vec, l, middlePoint, r);
+            }
+        }
+
+        private static void Merge(Vector vec, int l, int middlePoint, int r)
+        {
+            int sizeL = middlePoint - l + 1,
+                sizeR = r - middlePoint;
+
+            int[] tempL = new int[sizeL],
+                tempR = new int[sizeR];
+
+
+            int i, j;
+            for (i = 0; i < sizeL; ++i)
+                tempL[i] = vec[l + i];
+
+            for (j = 0; j < sizeR; ++j)
+                tempR[j] = vec[middlePoint + 1 + j];
+
+            i = 0;
+            j = 0;
+            int idx = l;
+
+            while (i < sizeL && j < sizeR)
+            {
+                if (tempL[i] <= tempR[j])
+                {
+                    vec[idx] = tempL[i];
+                    i++;
+                }
+                else
+                {
+                    vec[idx] = tempR[j];
+                    j++;
+                }
+                idx++;
+            }
+
+            while (i < sizeL)
+            {
+                vec[idx] = tempL[i];
+                i++;
+                idx++;
+            }
+
+            while (j < sizeR)
+            {
+                vec[idx] = tempR[j];
+                j++;
+                idx++;
+            }
+        }
+
+        #endregion
+    }
+}
